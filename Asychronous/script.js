3b7ed7fa5c65fdd28e023c3788b0e552489132a9
VenkateshNandhakumar <saivenkatesh@gmail.com>
2023-06-07 11:50:04 +0530

    StyleSheet Added

--- Asychronous/script.js
+++ Asychronous/script.js
@@ -1,10 +1,21 @@
 ////////////////////////////////////////////////////
 // Asychronous using XMLHttpRequest
-const request = new XMLHttpRequest();
-request.open("GET", `https://restcountries.com/v3.1/name/portugal`);
-request.send();
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  console.log(data.population);
-});
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    console.log(data.flags.png);
+  });
+};
+
+getCountryData("portugal");
+getCountryData("india");
+getCountryData("usa");
+
+/*
+  In the above code we couldn't get proper order of results. There will a minor delay in each call. 
+*/
